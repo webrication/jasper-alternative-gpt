dd881287d73f4bc539425fafb5d810643bd6c646
Shashank Jain <shashank@bytequests.com>
2024-05-03 13:58:03 +0530

    fixed

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Layout from '../components/Layout';
 import CardGrid from "../components/CardGrid";
 import {TEMPLATES} from "../constants/templates";
-import GetItNow from "../components/BuyIt";
 
 const HomePage: React.FC = () => {
 
